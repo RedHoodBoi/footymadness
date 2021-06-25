fa577f61fc1f1b16ba6e2e9564200f62d184c2f7
RedHoodBoi <43434738+RedHoodBoi@users.noreply.github.com>
2021-06-25 01:41:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -106,10 +106,8 @@ app.get('/api', (req, res) => {
     res.send({ message: 'Welcome to testtest!' });
 });
 const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+app.listen(8080, "0.0.0.0");
+
 
 
 /***/ }),
